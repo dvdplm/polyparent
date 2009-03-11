3b2ccc094ce3ccb541cfaa5d714d4fd82a06da99
David Palm <dpalm@elctech.com>
2009-03-11 19:11:22 +0100

    Initial version of the PolyParent

--- lib/polyparent.rb
+++ lib/polyparent.rb
@@ -1,1 +1,55 @@
-# Polyparent
+# Copyright (c) 2009 David Palm, Peer Allan
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+module BeefExtras #:nodoc:
+  module ActionController #:nodoc:
+    module PolyParent #:nodoc:
+
+      def self.included(base) # :nodoc:
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+        def parent_resources(*parents)
+          @parent_resources ||= parents
+        end
+      end
+
+      def parent_instance
+        parent_class && parent_class.find(parent_resource_id(parent_resource_type))
+      end
+
+      def parent_class
+        parent_resource_type && parent_resource_type.to_s.classify.constantize
+      end
+
+      def parent_resource_type
+        self.class.parent_resources.detect { |parent| parent_resource_id(parent) }
+      end
+
+      def parent_resource_id(parent)
+        request.path_parameters["#{ parent }_id"]
+      end
+      
+      private :parent_resource_id, :parent_resource_type
+    end
+  end
+end
--- spec/polyparent_spec.rb
+++ spec/polyparent_spec.rb
@@ -1,8 +1,75 @@
-require 'test_helper'
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-class PolyparentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class TestController < ActionController::Base
+  include BeefExtras::ActionController::PolyParent
+  parent_resources :user, :animal, :shoe, :string
+end
+
+describe "PolyParent" do
+  
+  def setup_request(params)
+    params = HashWithIndifferentAccess.new(params)
+    request = mock('web request', :path_parameters => params)
+    @controller.stub!(:request).and_return(request)
+  end
+  
+  before do
+    @controller = TestController.new
+  end
+  
+  describe "class methods" do
+    describe "keeps a list of resources (models) that can use this controller" do
+      it "keeps track of the resources that can use the controller in a nested manner" do
+        TestController.parent_resources.should == [:user, :animal, :shoe, :string]
+      end
+    end
+  end
+  
+  describe "instance_methods" do
+    before do
+      setup_request(:action => "show", :id => "123", :stuff => "somestring", :controller => "test_controller", :user_id => 321)
+    end
+    
+    it "extracts the ID of the parent resource instance from the params" do
+      @controller.send(:parent_resource_id, 'user').should == 321
+    end
+    
+    describe "extracts the type of the parent resource using the params and the parent resources" do
+      
+      it "does exactly that" do
+        @controller.send(:parent_resource_type).should == :user
+      end
+    
+      describe "copes with a whacky params hash" do
+        it "doesn't choke on other something_id in the params hash" do
+          setup_request(:action => "show", :id => "123", :stuff => "somestring", :controller => "test_controller", :ginger_id => 'dsas', 'id_entify' => :stuff, 'shoe_id' => 456 )
+          @controller.send(:parent_resource_type).should == :shoe
+        end
+      
+        it "doesn't choke on duplicates" do
+          setup_request(:shoe_id => "show", :shoe_id => "123", :shoe_id => "somestring")
+          @controller.send(:parent_resource_type).should == :shoe
+        end
+      end
+    end
+
+    describe "extracts the class of the parent resource using the params and the parent resources" do
+      
+      it "returns the class" do
+        setup_request(:string_id => 123)
+        @controller.parent_class.should == String
+      end
+    end
+    
+    describe "retrieves the instance" do
+      it "fetches the instance identified by the ID in the params hash from the DB" do
+        setup_request(:string_id => 123)
+        String.should_receive(:find).with(123).and_return "one two three"
+        @controller.parent_instance.should == "one two three"
+      end
+    end
+    
+    
+    
   end
 end
