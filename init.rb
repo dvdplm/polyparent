51192666847d0106d47338d0085d9877f7eea00a
David Palm <dpalm@elctech.com>
2009-03-11 19:10:55 +0100

    Init code

--- init.rb
+++ init.rb
@@ -1,1 +1,4 @@
-# Include hook code here
+if defined?(ActionController) and defined?(ActionController::Base)
+  require 'polyparent'
+  ActionController::Base.send :include, BeefExtras::ActionController::PolyParent
+end
